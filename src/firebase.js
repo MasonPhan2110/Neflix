76c8e142944889e7661c37532adb6ea3aafcb2bf
Nhat Minh <phannhatminh1099@example.com>
2023-06-16 23:39:13 +0700

    init

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDh1wHCtcO8l0wzgQmD-iKJbQI3MTs5XFE",
+    authDomain: "netflix-a37f9.firebaseapp.com",
+    projectId: "netflix-a37f9",
+    storageBucket: "netflix-a37f9.appspot.com",
+    messagingSenderId: "904436676158",
+    appId: "1:904436676158:web:e88580c0892de1987a436f",
+    measurementId: "G-WXDRXR5436"
+};
